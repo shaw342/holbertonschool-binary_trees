467c5e0a44f911b244012eba45b796246e5a5132
shaw342 <baruashawan41@gmail.com>
2023-05-15 16:32:00 +0200

    taks 5

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - function
+ * @node: node
+ * Return: 0
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+
+	if (node->parent == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+	return (0);
+
+}
