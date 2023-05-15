e23985363982e10ef4fa6885d92e878811f8ccab
shaw342 <baruashawan41@gmail.com>
2023-05-15 15:50:05 +0200

    adding task 4

---
+++ 1-main.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/*int main(void)
+{
+    binary_tree_t *root;
+
+    root = binary_tree_node(NULL, 98);
+    root->left = binary_tree_node(root, 12);
+    root->right = binary_tree_node(root, 402);
+    binary_tree_print(root);
+    printf("\n");
+    binary_tree_insert_left(root->right, 128);
+    binary_tree_insert_left(root, 54);
+    binary_tree_print(root);
+    return (0);
+}*/
+void binary_tree_print(const binary_tree_t *tree);
+void _binary_tree_delete(binary_tree_t *tree);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	binary_tree_t root;
+
+	root.n = 98;
+	root.parent = NULL;
+	root.left = NULL;
+	root.right = NULL;
+	binary_tree_insert_left(&root, 54);
+	binary_tree_insert_left(&root, 64);
+
+	binary_tree_print(&root);
+	_binary_tree_delete(root.left);
+	return (0);
+}
