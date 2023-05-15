d2eacc8dd79844ced924aa5425d5208e252801f5
shaw342 <baruashawan41@gmail.com>
2023-05-15 14:09:33 +0200

    free tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -4,7 +4,7 @@ void binary_tree_delete(binary_tree_t *tree)
 	if(tree == NULL)
 		return;
 
-	binary_tree_delete(parent->left);
-	binary_tree_delete(parent->right);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 	free(tree);
 }
