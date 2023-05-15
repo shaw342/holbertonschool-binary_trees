e01972ab72bf8390b4957146635262b8dcdc02e3
shaw342 <baruashawan41@gmail.com>
2023-05-15 14:01:08 +0200

    careate node left

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,6 +15,11 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	}
 	newNode = malloc(sizeof(binary_tree_t));
 
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+
 	newNode->parent =  parent;
 	newNode->n = value;
 	newNode->left = NULL;
