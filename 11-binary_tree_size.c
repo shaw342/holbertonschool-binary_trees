ae9a4dda63de0cd533fce42c107ebf355df60a03
shaw342 <baruashawan41@gmail.com>
2023-05-16 16:19:41 +0200

    task 10

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - function
+ * @tree: tree
+ * Return: size
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	else
+	{
+		return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+	}
+}
