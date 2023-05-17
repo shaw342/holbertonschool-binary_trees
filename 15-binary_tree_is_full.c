5ce5f4fb385cbbe54db58f147f29b5e15b750316
shaw342 <baruashawan41@gmail.com>
2023-05-17 13:41:41 +0200

    task 15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full - function
+ * @tree: tree
+ * Return: true or false
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+
+	if ((tree->left) && (tree->right))
+	{
+		return (binary_tree_is_full(tree->left)
+				&& binary_tree_is_full(tree->right));
+	}
+	return (0);
+}
